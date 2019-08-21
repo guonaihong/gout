79347a622d5fa124a5b73bded4475ab299961b12
guonaihong <guonaihong@qq.com>
2019-08-21 21:54:49 +0800

    fix test

--- group_test.go
+++ group_test.go
@@ -344,21 +344,26 @@ type testQuery struct {
 	Q2 int       `query:"q2" form:"q2"`
 	Q3 float32   `query:"q3" form:"q3"`
 	Q4 float64   `query:"q4" form:"q4"`
-	Q5 time.Time `query:"q5" form:"q5" time_format:"unix"`
-	Q6 time.Time `query:"q6" form:"q6" time_format:"unixNano"`
-	Q7 time.Time `query:"q7" form:"q7" time_format:"2006-01-02"`
+	Q5 time.Time `query:"q5" form:"q5" time_format:"unix" time_location:"Asia/Shanghai"`
+	Q6 time.Time `query:"q6" form:"q6" time_format:"unixNano" time_location:"Asia/Shanghai"`
+	Q7 time.Time `query:"q7" form:"q7" time_format:"2006-01-02" time_location:"Asia/Shanghai"`
 	testQuery2
 }
 
 func queryDefault() *testQuery {
+	loc, err := time.LoadLocation("Asia/Shanghai")
+	if err != nil {
+		panic(err.Error())
+	}
+
 	return &testQuery{
 		Q1: "v1",
 		Q2: 2,
 		Q3: 3.14,
 		Q4: 3.1415,
-		Q5: time.Date(2019, 7, 28, 14, 36, 0, 0, time.Local),
-		Q6: time.Date(2019, 7, 28, 14, 36, 0, 1000, time.Local),
-		Q7: time.Date(2019, 7, 28, 0, 0, 0, 0, time.Local),
+		Q5: time.Date(2019, 7, 28, 14, 36, 0, 0, loc),
+		Q6: time.Date(2019, 7, 28, 14, 36, 0, 1000, loc),
+		Q7: time.Date(2019, 7, 28, 0, 0, 0, 0, loc),
 		testQuery2: testQuery2{
 			Q8: 8,
 		},
@@ -374,7 +379,8 @@ func TestToQueryStruct(t *testing.T) {
 			err := c.ShouldBindQuery(&q2)
 			assert.NoError(t, err)
 
-			assert.Equal(t, q, &q2)
+			testQueryEqual(t, *q, q2)
+			//assert.Equal(t, q, &q2)
 		})
 
 		return router
@@ -397,6 +403,26 @@ func TestQueryString(t *testing.T) {
 	testQueryStringCore(t, s, true)
 }
 
+func testQueryEqual(t *testing.T, q1, q2 testQuery) {
+	//不用assert.Equal(t, q1, q2)
+	//assert.Equal 有个bug
+
+	assert.Equal(t, q1.Q1, q2.Q1)
+	assert.Equal(t, q1.Q2, q2.Q2)
+	assert.Equal(t, q1.Q3, q2.Q3)
+	assert.Equal(t, q1.Q4, q2.Q4)
+	assert.Equal(t, q1.Q8, q2.Q8)
+	if !q1.Q5.Equal(q2.Q5) {
+		t.Errorf("want(%s) got(%s)\n", q1.Q5, q2.Q5)
+	}
+	if !q1.Q6.Equal(q2.Q6) {
+		t.Errorf("want(%s) got(%s)\n", q1.Q6, q2.Q6)
+	}
+	if !q1.Q7.Equal(q2.Q7) {
+		t.Errorf("want(%s) got(%s)\n", q1.Q7, q2.Q7)
+	}
+}
+
 func testQueryStringCore(t *testing.T, qStr string, isPtr bool) {
 	q := queryDefault()
 	router := func() *gin.Engine {
@@ -404,9 +430,13 @@ func testQueryStringCore(t *testing.T, qStr string, isPtr bool) {
 		router.GET("/test.query", func(c *gin.Context) {
 			q2 := testQuery{}
 			err := c.ShouldBindQuery(&q2)
+
+			//todo
+			//fmt.Printf("------->q7(%t)\n", reflect.DeepEqual(q1.Q5, q2.Q5), reflect.DeepEqual(q1.Q6, q2.Q6), reflect.DeepEqual(q1.Q7, q2.Q7))
+			//fmt.Printf("%s:%s, %t\n", q1.Q7, q2.Q7, q1.Q7.Equal(q2.Q7))
 			assert.NoError(t, err)
 
-			assert.Equal(t, *q, q2)
+			testQueryEqual(t, *q, q2)
 		})
 
 		return router
@@ -437,7 +467,8 @@ func setupQuery(t *testing.T, q *testQuery) func() *gin.Engine {
 			err := c.ShouldBindQuery(&q2)
 			assert.NoError(t, err)
 
-			assert.Equal(t, *q, q2)
+			testQueryEqual(t, *q, q2)
+			//assert.Equal(t, *q, q2)
 		})
 
 		return router
