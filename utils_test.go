d7240253b4eb6a103d8fc427bed7cb45d69a6a44
guonaihong <guonaihong@qq.com>
2019-10-20 15:10:15 +0800

    提升代码测试覆盖度 #71
    修改utils_test.go 测试覆盖度到达100%

--- utils_test.go
+++ utils_test.go
@@ -5,7 +5,17 @@ import (
 	"testing"
 )
 
-func TestJoin(t *testing.T) {
+func TestUtil_Close(t *testing.T) {
+	assert.NoError(t, (&ReadCloseFail{}).Close())
+}
+
+func TestUtil_lastChar(t *testing.T) {
+	assert.Panics(t, func() {
+		lastChar("")
+	})
+}
+
+func TestUtil_Join(t *testing.T) {
 	urls := []string{
 		"http://127.0.0.1:43471/v1",
 	}
@@ -15,4 +25,31 @@ func TestJoin(t *testing.T) {
 	}
 
 	assert.Equal(t, joinPaths("", urls[0]), want[0])
+}
+
+type joinTest struct {
+	absolutePath string
+	relativePath string
+	need         string
+}
+
+func TestUtil_join(t *testing.T) {
+	//实际调用不会发生 join函数absolutePath和relativePath参数都为空的情况
+	assert.Equal(t, join("", ""), ".")
+}
+
+func TestUtil_joinPaths(t *testing.T) {
+	test := []joinTest{
+		{absolutePath: "", relativePath: "", need: ""},
+		{absolutePath: "https://www.aa.com", relativePath: "/a", need: "https://www.aa.com/a"},
+		{absolutePath: "http://www.bb.com", relativePath: "/b", need: "http://www.bb.com/b"},
+		{absolutePath: "www.bb.com", relativePath: "/b", need: "www.bb.com/b"},
+		{absolutePath: "www.bb.com", relativePath: "", need: "www.bb.com"},
+		{absolutePath: "", relativePath: "/a", need: "/a"},
+	}
+
+	for _, v := range test {
+		rv := joinPaths(v.absolutePath, v.relativePath)
+		assert.Equal(t, rv, v.need)
+	}
 }
