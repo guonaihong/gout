5e519d2b75874e383a0a08222e47a8dfe5454c7e
guonaihong <guonaihong@qq.com>
2019-11-16 19:26:28 +0800

    提升测试覆盖度 #81

--- encode/www_form_test.go
+++ encode/www_form_test.go
@@ -1,6 +1,8 @@
 package encode
 
 import (
+	"bytes"
+	"github.com/guonaihong/gout/core"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -10,10 +12,29 @@ func Test_WWWForm_New(t *testing.T) {
 	assert.NotNil(t, NewWWWFormEncode("h"))
 }
 
-func Test_WWWForm_Encode(t *testing.T) {
+type testWWWForm struct {
+	w    *WWWFormEncode
+	need string
 }
 
-func Test_WWWForm_Add(t *testing.T) {
+func Test_WWWForm_Encode(t *testing.T) {
+	var out bytes.Buffer
+
+	tests := []testWWWForm{
+		{NewWWWFormEncode(core.A{"k1", "v1", "k2", 2, "k3", 3.14}), "k1=v1&k2=2&k3=3.14"},
+		{NewWWWFormEncode(core.H{"k1": "v1", "k2": 2, "k3": 3.14}), "k1=v1&k2=2&k3=3.14"},
+	}
+
+	for _, v := range tests {
+		v.w.Encode(&out)
+		assert.Equal(t, out.String(), v.need)
+		out.Reset()
+	}
+
+	// 出错
+	w := NewWWWFormEncode("fail")
+	err := w.Encode(&out)
+	assert.Error(t, err)
 }
 
 func Test_WWWForm_Name(t *testing.T) {
