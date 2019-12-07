e2280da68c6a5647d63920f56e31261b84435b2c
guonaihong <guonaihong@qq.com>
2019-12-07 11:12:43 +0800

    提升代码测试覆盖度
    fix closeRequest复制http header的问题
    提升req.go的覆盖度到100%

--- bench/req.go
+++ bench/req.go
@@ -13,7 +13,7 @@ func cloneRequest(r *http.Request) (*http.Request, error) {
 	r0.Header = make(http.Header, len(r.Header))
 
 	for k, h := range r.Header {
-		r.Header[k] = append([]string(nil), h...)
+		r0.Header[k] = append([]string(nil), h...)
 	}
 
 	r0.Body, err = r.GetBody()
