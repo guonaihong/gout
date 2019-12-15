8915659fed47c229531868c3e7ced05facff41d4
guonaihong <guonaihong@qq.com>
2019-12-15 12:06:26 +0800

    retry 更新测试代码

--- retry_test.go
+++ retry_test.go
@@ -10,10 +10,27 @@ import (
 	"time"
 )
 
-func setup_retry() *gin.Engine {
+const (
+	retry_Count        = 3
+	retry_doesNotExist = ":6364"
+)
+
+func setup_retry_fail() *gin.Engine {
+	router := gin.New()
+
+	var done chan struct{}
+	router.GET("/", func(c *gin.Context) {
+		<-done
+	})
+
+	return router
+}
+
+func setup_retry_ok() *gin.Engine {
 	router := gin.New()
 
 	router.GET("/", func(c *gin.Context) {
+		c.String(200, "ok")
 	})
 
 	return router
@@ -40,6 +57,7 @@ func Test_Retry_sleep(t *testing.T) {
 	r := Retry{attempt: 100}
 	r.init()
 
+	// 方便画出曲线图
 	for i := 0; i < r.attempt; i++ {
 		sleep := r.getSleep()
 		fmt.Printf("%d\n", sleep)
@@ -54,18 +72,39 @@ func Test_Retry_init(t *testing.T) {
 	r1.init()
 	assert.Equal(t, r1, r)
 }
-
 func Test_Retry_Do(t *testing.T) {
-	router := setup_retry()
+	// 6364是随便写的一个端口，如果CI/CD这台机器上有这个端口，就要换个不存在的
+	router := setup_retry_fail()
 	ts := httptest.NewServer(http.HandlerFunc(router.ServeHTTP))
+	urls := []string{ts.URL, retry_doesNotExist}
+	// 测试全部超时的情况
+	for _, u := range urls {
+		err := GET(u).
+			SetTimeout(10 * time.Millisecond).
+			Debug(true).
+			Filter().
+			Retry().
+			Attempt(retry_Count).
+			WaitTime(time.Millisecond * 10).
+			MaxWaitTime(time.Millisecond * 50).
+			Do()
+		assert.Error(t, err)
+	}
 
-	err := GET(ts.URL).
-		Filter().
-		Retry().
-		Attempt(3).
-		WaitTime(time.Millisecond * 10).
-		MaxWaitTime(time.Millisecond * 800).
-		Do()
-
-	assert.NoError(t, err)
+	// 测试正确的情况
+	router = setup_retry_ok()
+	ts = httptest.NewServer(http.HandlerFunc(router.ServeHTTP))
+	urls = []string{ts.URL}
+	for _, u := range urls {
+		err := GET(u).
+			SetTimeout(10 * time.Millisecond).
+			Debug(true).
+			Filter().
+			Retry().
+			Attempt(retry_Count).
+			WaitTime(time.Millisecond * 10).
+			MaxWaitTime(time.Millisecond * 50).
+			Do()
+		assert.NoError(t, err)
+	}
 }
--- version.go
+++ version.go
@@ -1,3 +1,3 @@
 package gout
 
-const Version = "v0.0.5-dev-2019-12-09"
+const Version = "v0.0.5-dev-2019-12-15"
