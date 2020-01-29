a79a50102e72acb5970cf28e799cd2f43ff5ab4e
guonaihong <guonaihong@qq.com>
2020-01-29 21:58:46 +0800

    修改SetYAML函数 (#176)

--- _example/12-yaml.go
+++ _example/12-yaml.go
@@ -12,6 +12,86 @@ type data struct {
 	Data string `json:"data xml:"data" yaml:"data"`
 }
 
+func useStruct() {
+	var rsp data
+	code := 200
+
+	err := gout.POST(":8080/test.yaml").
+		Debug(true).
+		SetYAML(data{Id: 3, Data: "test data"}).
+		BindYAML(&rsp).
+		Code(&code).
+		Do()
+
+	if err != nil || code != 200 {
+		fmt.Printf("%v:%d\n", err, code)
+	}
+}
+
+func useMap() {
+	var rsp data
+	code := 200
+
+	err := gout.POST(":8080/test.yaml").
+		Debug(true).
+		SetYAML(gout.H{"id": 3, "data": "test data"}).
+		BindYAML(&rsp).
+		Code(&code).
+		Do()
+
+	if err != nil || code != 200 {
+		fmt.Printf("%v:%d\n", err, code)
+	}
+}
+
+func useString() {
+	var rsp data
+	code := 200
+
+	err := gout.POST(":8080/test.yaml").
+		Debug(true).
+		SetYAML(`
+id: 3
+data: test data
+`).
+		BindYAML(&rsp).
+		Code(&code).
+		Do()
+
+	if err != nil || code != 200 {
+		fmt.Printf("%v:%d\n", err, code)
+	}
+}
+
+func useBytes() {
+	var rsp data
+	code := 200
+
+	err := gout.POST(":8080/test.yaml").
+		Debug(true).
+		SetYAML([]byte(`
+id: 3
+data: test data
+`)).
+		BindYAML(&rsp).
+		Code(&code).
+		Do()
+
+	if err != nil || code != 200 {
+		fmt.Printf("%v:%d\n", err, code)
+	}
+}
+
+func main() {
+	go server()
+	time.Sleep(time.Millisecond * 500) //sleep下等服务端真正起好
+
+	useStruct()
+	useMap()
+	useString()
+	useBytes()
+}
+
 func server() {
 	router := gin.Default()
 
@@ -26,23 +106,4 @@ func server() {
 	})
 
 	router.Run()
-}
-
-func main() {
-	var rsp data
-	go server()
-	time.Sleep(time.Millisecond * 500) //sleep下等服务端真正起好
-
-	code := 200
-
-	err := gout.POST(":8080/test.yaml").
-		Debug(true).
-		SetYAML(data{Id: 3, Data: "test data"}).
-		BindYAML(&rsp).
-		Code(&code).
-		Do()
-
-	if err != nil || code != 200 {
-		fmt.Printf("%v:%d\n", err, code)
-	}
 }
