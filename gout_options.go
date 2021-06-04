55ffd214969a11fa49acf84f1d890b9eb214b79d
guonaihong <guonaihong@qq.com>
2021-06-04 21:55:16 +0800

    开发#288

--- gout_options.go
+++ gout_options.go
@@ -2,13 +2,16 @@ package gout
 
 import (
 	"crypto/tls"
+	"net"
 	"net/http"
 )
 
+// 配置
 type options struct {
 	hc *http.Client
 }
 
+// apply接口
 type Option interface {
 	apply(*options)
 }
@@ -37,6 +40,38 @@ func (c *client) apply(opts *options) {
 	opts.hc = (*http.Client)(c)
 }
 
+// 传递http.Client
 func WithClient(c *http.Client) Option {
 	return (*client)(c)
+}
+
+type fromIP func(netw, addr string) (net.Conn, error)
+
+func (f fromIP) apply(opts *options) {
+	tr, ok := opts.hc.Transport.(*http.Transport)
+	if !ok {
+		tr = &http.Transport{}
+	}
+
+	tr.Dial = f
+}
+
+// 指定发送的ip
+func WithFromIP(IP string) Option {
+	dial := func(network, addr string) (net.Conn, error) {
+		lAddr, err := net.ResolveTCPAddr(network, IP+":0")
+		if err != nil {
+			return nil, err
+		}
+
+		//被请求的地址
+		rAddr, err := net.ResolveTCPAddr(network, addr)
+		if err != nil {
+			return nil, err
+		}
+
+		return net.DialTCP(network, lAddr, rAddr)
+	}
+
+	return fromIP(dial)
 }
