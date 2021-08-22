b0a3fc25922e435dd7adb582b035dfc933e630a7
guonaihong <guonaihong@qq.com>
2021-08-22 17:16:14 +0800

    完成该功能

--- encode/query_test.go
+++ encode/query_test.go
@@ -1,14 +1,17 @@
 package encode
 
 import (
-	"github.com/guonaihong/gout/setting"
-	"github.com/stretchr/testify/assert"
 	"net/http"
 	"strconv"
 	"testing"
 	"time"
+
+	"github.com/guonaihong/gout/core"
+	"github.com/guonaihong/gout/setting"
+	"github.com/stretchr/testify/assert"
 )
 
+// 测试[]string类型
 func TestQueryStringSlice(t *testing.T) {
 	req, _ := http.NewRequest("GET", "/", nil)
 
@@ -21,6 +24,7 @@ func TestQueryStringSlice(t *testing.T) {
 	assert.Equal(t, q.End(), "q1=v1&q2=v2&q3=v3")
 }
 
+// 测试map[string]interface{}
 func TestQueryMap(t *testing.T) {
 	req, _ := http.NewRequest("GET", "/", nil)
 
@@ -41,6 +45,7 @@ type testQuery struct {
 	Q5 time.Time `query:"q5" time_format:"unixNano"`
 }
 
+// 测试结构体
 func TestQueryStruct(t *testing.T) {
 	req, _ := http.NewRequest("GET", "/", nil)
 
@@ -54,4 +59,30 @@ func TestQueryStruct(t *testing.T) {
 	assert.NoError(t, err)
 
 	assert.Equal(t, q.End(), "q1=v1&q2=v2&q3=v3&q4="+strconv.FormatInt(unixTime.Unix(), 10)+"&q5="+strconv.FormatInt(unixNano.UnixNano(), 10))
+}
+
+// 结构体带[]string
+type queryWithSlice struct {
+	A []string `query:"a"`
+	B string   `query:"b"`
+}
+
+func TestQueryFieldWithSlice(t *testing.T) {
+
+	for _, v := range []interface{}{
+		queryWithSlice{A: []string{"1", "2", "3"}, B: "b"},
+		core.H{"a": []string{"1", "2", "3"}, "b": "b"},
+		core.A{"a", []string{"1", "2", "3"}, "b", "b"},
+	} {
+
+		req, _ := http.NewRequest("GET", "/", nil)
+
+		q := NewQueryEncode(req, setting.Setting{})
+
+		err := Encode(v, q)
+
+		assert.NoError(t, err)
+
+		assert.Equal(t, q.End(), "a=1&a=2&a=3&b=b")
+	}
 }
