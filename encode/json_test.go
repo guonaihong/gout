b2c5a95c5b373519c6f05aa0ac2d4a6889fbb94e
oak <ch@beituyun.com>
2022-04-20 10:38:38 +0800

    fix: 修复json.Marshal自动替换< > &

--- encode/json_test.go
+++ encode/json_test.go
@@ -28,12 +28,12 @@ func TestJSONEncode_Encode(t *testing.T) {
 	need := testJSON{
 		I: 100,
 		F: 3.14,
-		S: "test encode json > < &",
+		S: "test encode json > < & \u0026",
 	}
 
 	out := bytes.Buffer{}
 
-	s := `{"I" : 100, "F" : 3.14, "S":"test encode json > < &"}`
+	s := `{"I" : 100, "F" : 3.14, "S":"test encode json > < & \u0026"}`
 	data := []interface{}{need, &need, s, []byte(s)}
 	for _, v := range data {
 		j := NewJSONEncode(v)
