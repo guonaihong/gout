d535eebf5ac11c375e17cfb310d91d0694652fe7
simpleapples <zangzhiya@gmail.com>
2023-12-13 14:00:28 +0000

    feat: set content-type only when it doesn't exist on user custom headers

--- dataflow/req.go
+++ dataflow/req.go
@@ -123,6 +123,10 @@ func (r *Req) addDefDebug() {
 }
 
 func (r *Req) addContextType(req *http.Request) {
+	if req.Header.Get("Content-Type") != "" {
+		return
+	}
+
 	if r.wwwForm != nil {
 		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 	}
